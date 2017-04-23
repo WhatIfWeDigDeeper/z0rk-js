abeee2f6830fa3b45b3a48ec61392e019da98da5
Gregory <whatifwedigdeeper@gmail.com>
2017-04-23 13:09:24 -0500

    Change command processing to a pure function and removed jQuery parameter.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,12 @@
-function parseCommand(text, $line) {
+function handleCommand(text) {
     if (text === 'open mailbox') {
-        $line.append('<p>Opening the small mailbox reveals a leaflet.</p><br>')
+        return { message: '<p>Opening the small mailbox reveals a leaflet.</p><br>' };
     } else if (text === 'read leaflet') {
-        let score = +$('#score').text();
-        $('#score').text(++score);
-        $line.append(`<p>
-(Taken)<br>
-"WELCOME TO ZORK!<br>
-<br>
+        return {
+            score: 1,
+            message: `<p>(Taken)<br>"WELCOME TO ZORK!<br><br>
 ZORK is a game of adventure, danger, and low cunning. In it you will explore
 some of the most amazing territory ever seen by mortals. No computer should be without one!"
-</p><br>`)
+</p><br>`};
     }
 }
